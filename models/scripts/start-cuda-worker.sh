4e15e46469943182cb1f094e4694117e9cf5acde
kac487 <kyleacdev@gmail.com>
2023-07-26 11:36:18 -0700

    Update compose to use local network (temp) update start scripts and dockerfiles

--- models/scripts/start-cuda-worker.sh
+++ models/scripts/start-cuda-worker.sh
@@ -3,11 +3,18 @@
 python3 -c "from huggingface_hub._login import _login; import os; _login(token=os.getenv('HF_TOKEN'), add_to_git_credential=False)";
 
 python3 \
--m fastchat.serve.vllm_worker \
---model-names ${FASTCHAT_WORKER_MODEL_NAMES:-Llama-2-7b-chat-hf} \
+-m fastchat.serve.model_worker \
+--model-names Llama-2-7b-chat-hf \
 --model-path meta-llama/Llama-2-7b-chat-hf \
---worker-address http://fastchat-model-worker-cuda:21002 \
---controller-address http://fastchat-controller:21001 \
---device cuda \
+--worker-address http://localhost:21002 \
+--controller-address http://localhost:21001 \
+--load-8bit \
+--cpu-offloading \
+--device "cuda" \
+--max-gpu-memory "4G" \
 --host "0.0.0.0" \
---port 21002
+--port 21002;
+
+
+# --worker-address http://fastchat-model-worker-cuda:21002 \
+# --controller-address http://fastchat-controller:21001 \
