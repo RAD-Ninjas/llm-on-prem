d48f18318073330a624aaa95a983a80350bcbd45
Chepe Ruiz <chepe.ruiz@gmail.com>
2023-07-27 01:55:54 -0700

    added metal support

--- models/scripts/start-cuda-worker.sh
+++ models/scripts/start-cuda-worker.sh
@@ -3,17 +3,17 @@
 python3 -c "from huggingface_hub._login import _login; import os; _login(token=os.getenv('HF_TOKEN'), add_to_git_credential=False)";
 
 python3 \
--m fastchat.serve.model_worker \
---model-names ${MODEL_NAME} \
---model-path ${MODEL_PATH} \
---worker-address http://fastchat-model-worker-cuda:21002 \
---controller-address http://fastchat-controller:21001 \
---load-8bit \
---cpu-offloading \
---device "cuda" \
---host "0.0.0.0" \
---port 21002;
-
+ -m fastchat.serve.model_worker \
+ --model-names ${MODEL_NAME} \
+ --model-path ${MODEL_PATH} \
+ --worker-address http://fastchat-model-worker-cuda:21002 \
+ --controller-address http://fastchat-controller:21001 \
+ --load-8bit \
+ --cpu-offloading \
+ --device "cuda" \
+ --host "0.0.0.0" \
+ --port 21002;
+ 
 
 # --worker-address http://fastchat-model-worker-cuda:21002 \
 # --controller-address http://fastchat-controller:21001 \
