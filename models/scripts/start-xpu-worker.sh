c150c30a9e3bf0f15ec877759915bb6a630063e0
kac487 <kyleacdev@gmail.com>
2023-07-26 22:07:00 -0700

    formatting to help on windows

--- models/scripts/start-xpu-worker.sh
+++ models/scripts/start-xpu-worker.sh
@@ -3,14 +3,14 @@
 python3 -c "from huggingface_hub._login import _login; import os; _login(token=os.getenv('HF_TOKEN'), add_to_git_credential=False)";
 
 python3 \
--m fastchat.serve.model_worker \
---model-names ${MODEL_NAME} \
---model-path ${MODEL_PATH} \
---worker-address http://fastchat-model-worker-xpu:21002 \
---controller-address http://fastchat-controller:21001 \
---device "xpu" \
---host "0.0.0.0" \
---port 21002;
+ -m fastchat.serve.model_worker \
+ --model-names ${MODEL_NAME} \
+ --model-path ${MODEL_PATH} \
+ --worker-address http://fastchat-model-worker-xpu:21002 \
+ --controller-address http://fastchat-controller:21001 \
+ --device "xpu" \
+ --host "0.0.0.0" \
+ --port 21002;
 
 
 # --worker-address http://fastchat-model-worker-cpu:21002 \
